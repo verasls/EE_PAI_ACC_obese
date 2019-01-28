f90a01ab6367ff1d998eb7b4d61498ac7d23d577
Lucas Veras <lucasdsveras@gmail.com>
2019-01-28 13:39:19 +0000

    Corrects kcal equation

--- R/get_kcal.R
+++ R/get_kcal.R
@@ -15,7 +15,7 @@ get_kcal <- function(df, ID_num) {
   
   ID_df$kcal <- NA
   for (i in 1:nrow(ID_df)) {
-    ID_df$kcal[i] <- (3.941 * ID_df$V.O2[i]) + (1.106 * ID_df$V.CO2[i])
+    ID_df$kcal[i] <- (3.941 * (ID_df$V.O2[i] / 1000)) + (1.106 * (ID_df$V.CO2[i] / 1000))
   }
   return(ID_df)
 }
